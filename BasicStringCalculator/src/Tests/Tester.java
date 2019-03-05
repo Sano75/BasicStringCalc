1f7dcd4d9b89e07ce4fb838d5a3731ebc4b55e39
Sano75 <43172203+Sano75@users.noreply.github.com>
2019-03-05 22:02:47 +0100

    Test Updates
    Modulus Tests and equations added

--- BasicStringCalculator/src/Tests/Tester.java
+++ BasicStringCalculator/src/Tests/Tester.java
@@ -29,17 +29,84 @@ public class Tester{
 		assertEquals(expect, actual, RANGE);
 	}
 	
+	
 	@Test
 	public void test2() {
 		String equation = "3+4*5/4-3";
-		double expect = 2.5;
+		double expect = 5.0;
 		
 		double actual = calc.Calculate(equation);
 		
 		assertEquals(expect, actual, RANGE);
 	}
 	
+	@Test
+	public void test3() {
+		String equation = "5%3/33";
+		double expect = 0.0909;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
 	
+	@Test
+	public void test4() {
+		String equation = "33/5%3";
+		double expect = 0.599999;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
+	
+	@Test
+	public void test5() {
+		String equation = "33/5%3+5";
+		double expect = 5.599999;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
+	@Test
+	public void test6() {
+		String equation = "33/5%3*10";
+		double expect = 5.999999;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
+	@Test
+	public void test7() {
+		String equation = "10*33/5%3";
+		double expect = 5.999999;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
+	
+	@Test
+	public void test8() {
+		String equation = "5%3*33/2";
+		double expect = 33.0;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
+	@Test
+	public void test9() {
+		String equation = "5%3*33";
+		double expect = 66.0;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
+		
 	@Test
 	public void whenStringOneGetDoubleOne() {
 		
@@ -118,5 +185,15 @@ public class Tester{
 		assertEquals(expect, actual, RANGE);
 	}
 	
+	
+	@Test
+	public void modulusCalc() {
+		String equation = "9%5";
+		double expect = 4.0;
+		
+		double actual = calc.Calculate(equation);
+		
+		assertEquals(expect, actual, RANGE);
+	}
 
 }
